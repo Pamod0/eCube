7b1eaa9a9e9b129458b9adb73ea9b925be39155d
Pamod <pamodchandulapeiris@gmail.com>
2024-01-05 12:07:15 +0530

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+/* mobile navbar */
+
+/* hamburger icon */
+const menuBtn = document.querySelector('.hamburger-btn-wrapper');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+})
+
+/* Open when someone clicks on the span element */
+/*function openNav() {
+    document.getElementById('myNav').style.width = '100%';
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  /*function closeNav() {
+    document.getElementById('myNav').style.width = '0%';
+  }*/
